69fdbb8df98fa3e0f8d4d008c1226272a50ed30d
Leroy <4c65726f79@gmail.com>
2018-10-14 20:40:55 +0200

    Testing -> Failing

--- src/views/Public/Present.vue
+++ src/views/Public/Present.vue
@@ -1,14 +1,36 @@
 <template>
-  <div class="">
-    sd
+  <div>
+    <div>sd</div>
+    <input type="text" :value="data.UserId"/>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Video',
-  components: {
+  data () {
+    return {
 
+    }
+  },
+  apollo: {
+    WebhookByUser: {
+      query: gql`
+      query WebhookByUser($token: String) {
+        WebhookByUser(userid: $token) {
+          _id
+          UserId
+          intTime
+          WebhookConntent
+        }
+      }`,
+      variables: {
+        token: `sdsdsdsdsd`
+      },
+      update(data) {
+        console.log(data)
+      }
+    }
   }
 }
 
